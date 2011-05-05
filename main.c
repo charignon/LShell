45125e6212815d507e1e5f0d2fc24786b22e1ea6
Laurent Charignon <l.charignon@gmail.com>
2011-05-05 13:55:31 +0200

    Getting the user input correctly

--- main.c
+++ main.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main ()
 {
 
-char * command = (char * ) malloc ( sizeof(char) * 26);
+char * command = (char *) malloc (sizeof (char) * 25);
+char * buffer  = (char *) malloc (sizeof (char) * 25);
+int size_command = 0;
+int n = 0;
+
+
+
+
 printf("Welcome to LSell\n");
-int n = read(1,command,25*sizeof(char));
-printf("%s \n%d \n",command,n);
+for(;;)
+{ 
+  size_command = 0;
+  n = 0;
+  for(;;)
+  {
+    n = read(1,buffer,25*sizeof(char));
+    printf("************************* n  = %d *******************\n",n);
+    printf("%s %s %d \n", command,buffer,size_command+n);
+    command = (char*) realloc(command,(size_command+n)*sizeof(char));
+    command = (char*)strncat(command,buffer,sizeof(char)*n);
+    size_command+=n;
+    if (n < 25 || buffer[24]=='\n')
+      break;
+  }
+  break;
+}
+
+free(buffer);
 free(command);
 
 
 
-
 return EXIT_SUCCESS;
+
 
 
 
