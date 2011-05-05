040d56c6e1a70e9e85347362761bc6dc349764c3
Laurent Charignon <l.charignon@gmail.com>
2011-05-05 14:15:31 +0200

    ls working

--- main.c
+++ main.c
@@ -2,17 +2,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <pthread.h>
+#include <sys/wait.h>
 
 int main ()
 {
 
 char * command = (char *) malloc (sizeof (char) * 25);
 char * buffer  = (char *) malloc (sizeof (char) * 25);
+char * args []= {"ls",NULL};
 int size_command = 0;
 int n = 0;
-
-
+int pid ;
 
 
 printf("Welcome to LSell\n");
@@ -23,21 +24,31 @@ for(;;)
   for(;;)
   {
     n = read(1,buffer,25*sizeof(char));
-    printf("************************* n  = %d *******************\n",n);
-    printf("%s %s %d \n", command,buffer,size_command+n);
     command = (char*) realloc(command,(size_command+n)*sizeof(char));
     command = (char*)strncat(command,buffer,sizeof(char)*n);
     size_command+=n;
+
     if (n < 25 || buffer[24]=='\n')
       break;
   }
+  //We have a command to execute
+  printf("%s\n",command); 
+  command[size_command-1]='\0';
+  switch(pid = fork())
+  {
+  case -1 : perror("fork");break;
+        
+  case 0 : 
+            execvp(command,args);
+            break;
+  default : wait(0);break;
+  }
+
   break;
 }
 
 free(buffer);
 free(command);
-
-
 
 return EXIT_SUCCESS;
 
